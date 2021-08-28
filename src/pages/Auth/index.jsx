5d11979ddcaca340b8eb30c83c09c2d1b345074a
Mona Lisa <mykolakratasyk@gmail.com>
2021-08-29 00:28:54 +0300

    add pages

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Block } from "components";
+import "./Auth.scss";
+
+const Auth = () => {
+  return (
+    <div>
+      <Block>
+        <Button type="primary" size="large">
+          this is btn
+        </Button>
+      </Block>
+    </div>
+  );
+};
+
+
+
+
+export default Auth;
