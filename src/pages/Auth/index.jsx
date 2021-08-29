588f1956580ec94c204b30e5c1823c10c247af97
Mona Lisa <mykolakratasyk@gmail.com>
2021-08-29 16:26:34 +0300

    add form

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Button, Block } from "components";
+import { Form, Icon, Input,  Checkbox } from 'antd';
 import "./Auth.scss";
 
 const Auth = () => {
   return (
-    <div>
+    <section className="auth">
+     
       <Block>
+      <div className="auth__block">
+        <h2>Войти в аккаунт</h2>
+        <p>Please, login in</p>
+      </div>
         <Button type="primary" size="large">
-          this is btn
+          Connect...
         </Button>
       </Block>
-    </div>
+    </section>
   );
 };
-
-
-
 
 export default Auth;
